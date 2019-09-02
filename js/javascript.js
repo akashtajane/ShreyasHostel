e7db942a838c6b6615c4de82cc316576227789d2
Akash Tajane <akashtajane007@gmail.com>
2019-09-02 13:05:59 +0530

    Publishing my first website
    Im happy

---
+++ js/javascript.js
@@ -0,0 +1,37 @@
+function slideshow(){
+				var x = document.getElementById('check-class')
+				if(x.style.display === "none"){
+					x.style.display="block";
+				}else{
+					x.style.display="none";
+				}
+			}
+
+		
+function show_hidden1(){
+				var click = document.getElementById("drop-content");
+				if(click.style.display === "none"){
+					click.style.display = "block";
+				}else{
+					click.style.display = "none";
+				}
+			}
+		
+function show_hidden2(){
+				var click = document.getElementById("slide-content");
+				if(click.style.display === "none"){
+					click.style.display = "block";
+				}else{
+					click.style.display = "none";
+				}
+			}
+
+function show_hidden3(){
+				var click = document.getElementById("show-content");
+				if(click.style.display === "none"){
+					click.style.display = "block";
+				}else{
+					click.style.display = "none";
+				}
+
+			}
